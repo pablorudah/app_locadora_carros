bd13c86b4249f7343b4f6a8f8dc8bf7d968ffc28
pablo.ribeiro <pablorudah@gmail.com>
2023-08-10 00:24:04 -0300

    implementando migrations e configurando conexao com o DB

--- database/migrations/2023_08_10_023748_create_carros_table.php
+++ database/migrations/2023_08_10_023748_create_carros_table.php
@@ -15,7 +15,14 @@ class CreateCarrosTable extends Migration
     {
         Schema::create('carros', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('modelo_id');
+            $table->string('placa', 10)->unique();
+            $table->boolean('disponivel');
+            $table->integer('km');
             $table->timestamps();
+
+            //foreign key (constraints)
+            $table->foreign('modelo_id')->references('id')->on('modelos');
         });
     }
 
@@ -26,6 +33,10 @@ class CreateCarrosTable extends Migration
      */
     public function down()
     {
+        Schema::table('carros', function (Blueprint $table) {
+            $table->dropForeign(['modelo_id']);
+        });
+
         Schema::dropIfExists('carros');
     }
 }
